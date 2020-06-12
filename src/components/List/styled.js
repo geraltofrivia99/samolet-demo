5477c78e92ff97ca3cac25970abcde86b958ce7c
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2020-06-12 17:00:29 +0700

    finish testing demo

---
+++ src/components/List/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+    padding: 0 5%;
+    height: 100vh;
+`;
+
+export const SearchContainer = styled.div`
+    /* margin: 1% 40% 2% 0; */
+    width: 60%;
+    height: 7%;
+    padding-top: 10px;
+`;
+
+export const TableContainer = styled.div`
+    width: min(70%, 1600px);
+    height: 93%
+`;
